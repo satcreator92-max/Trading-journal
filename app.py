e7bfae9fe2c3638caef63b392e7164a2c6726076
satcreator92-max <sat.creator92@gmail.com>
2026-09-25 20:31:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.sidebar.header("📁 Journal Selection")
 journal_account = st.sidebar.selectbox("Select Account", [
     "Personal / Old Journal", 
     "50Cr Prop Firm Account",
-    "New Strategy Test" 
+    "GBPCAD Test" 
 ])
 
 # தேர்ந்தெடுக்கப்பட்ட கணக்கிற்கு ஏற்ப டேட்டா ஃபைல் மாறும்
